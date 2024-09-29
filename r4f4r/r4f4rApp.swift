39569c2bd86186a993ac27a99fc9dd6122ec76a4
jwan <jwanfaiisal@outlook.com>
2024-09-29 10:28:04 +0300

    Initial Commit

---
+++ r4f4r/r4f4rApp.swift
@@ -0,0 +1,17 @@
+//
+//  r4f4rApp.swift
+//  r4f4r
+//
+//  Created by Jwan Faisal Alameer on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct r4f4rApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ r4f4rTests/r4f4rTests.swift
@@ -0,0 +1,17 @@
+//
+//  r4f4rTests.swift
+//  r4f4rTests
+//
+//  Created by Jwan Faisal Alameer on 26/03/1446 AH.
+//
+
+import Testing
+@testable import r4f4r
+
+struct r4f4rTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
